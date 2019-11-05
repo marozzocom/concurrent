d5879d8744bb69ca0595be837080089b3009f146
Ilkka Hartikainen <ilkka.hartikainen@gmail.com>
2019-11-05 14:16:17 +0200

    update static file location

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path")
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, "dist")))
+app.use("/dist", express.static(path.join(__dirname, "dist")))
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
